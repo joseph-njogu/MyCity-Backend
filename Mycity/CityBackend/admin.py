5f7791329e821d4783003c6b5ef3352aee6c7a32
joseph-njogu <josephnjogu487@gmail.com>
2022-05-17 18:06:01 +0300

    Add models

--- Mycity/CityBackend/admin.py
+++ Mycity/CityBackend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Place)
+@admin.register(UserData)
+
+class PlaceAdmin(admin.OSMGeoAdmin):
+   pass

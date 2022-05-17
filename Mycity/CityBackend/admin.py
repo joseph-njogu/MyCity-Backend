c73d3ff65742d361a168875aea102e3779811b13
joseph-njogu <josephnjogu487@gmail.com>
2022-05-17 21:14:22 +0300

    Update snippet

--- Mycity/CityBackend/admin.py
+++ Mycity/CityBackend/admin.py
@@ -3,6 +3,8 @@ from .models import *
 
 @admin.register(Place)
 @admin.register(UserData)
+@admin.register(Packing)
+@admin.register(ParkingInfo)
 
 class PlaceAdmin(admin.OSMGeoAdmin):
    pass
--- Mycity/Mycity/settings.py
+++ Mycity/Mycity/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CityBackend',
+    'rest_framework',
+    'rest_framework.uthtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+)
+}

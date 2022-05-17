52ba9f97a3fa6cfb443e98bb04ec48df72fff0ed
joseph-njogu <josephnjogu487@gmail.com>
2022-05-17 23:56:14 +0300

    Redeploy

--- Mycity/settings.py
+++ Mycity/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-ax=su=08im-@*vak2r+@z*n+iye1=phk!%@k4bqz8_)8951)4n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition

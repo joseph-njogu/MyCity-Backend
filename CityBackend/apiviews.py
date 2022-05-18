fb2a539fe7b5dc2505f5a9736907a0eb8e368a77
joseph-njogu <josephnjogu487@gmail.com>
2022-05-18 13:39:21 +0300

    URLs to end points updated

--- CityBackend/apiviews.py
+++ CityBackend/apiviews.py
@@ -9,8 +9,8 @@ from .models import *
 from .serializers import *
 
 class UserCreate(generics.CreateAPIView):
-	authentication_classes = ()
-	permission_classes = ()
+	# authentication_classes = ()
+	# permission_classes = ()
 	serializer_class = UserSerializer
 
 class LoginView(APIView):
@@ -20,11 +20,11 @@ class LoginView(APIView):
 	def post(self, request,):
 		username = request.data.get("username")
 		password = request.data.get("password")
-		user = authenticate(username=username, password=password)
-		if user:
-			return Response({"token": user.auth_token.key})
-		else:
-			return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
+		# user = authenticate(username=username, password=password)
+		# if user:
+		# 	return Response({"token": user.auth_token.key})
+		# else:
+		# 	return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
 
 class UserViewSet(viewsets.ModelViewSet):
     """
